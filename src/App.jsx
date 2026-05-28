f78d40e68b60750866ffaee7d31aa3bfe003b686
AbhishekGore05 <abhishekgore806@gmail.com>
2026-05-28 15:25:41 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <h1>React Deployement with Github</h1>
+        <p>Welcome to React Deployement with Github</p>
+      </div>
+      </>
+  )
+}
+
+export default App
